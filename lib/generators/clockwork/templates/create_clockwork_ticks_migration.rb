81f04ee1e52a66613ce373f3f36d4cb101f5a808
Jaco Pretorius <jaco.pretorius@betterment.com>
2017-06-06 12:23:41 -0400

    Use a migration model to ensure we use the correct timezones when inserting the default row

--- lib/generators/clockwork/templates/create_clockwork_ticks_migration.rb
+++ lib/generators/clockwork/templates/create_clockwork_ticks_migration.rb
@@ -1,14 +1,13 @@
 class CreateClockworkTicks < ActiveRecord::Migration
+  class ClockworkTick < ActiveRecord::Base; end
+
   def self.up
     create_table :clockwork_ticks, force: true do |table|
       table.datetime :processed_at, null: false
       table.timestamps null: false
     end
 
-    execute <<-SQL
-      INSERT INTO clockwork_ticks (processed_at, created_at, updated_at)
-      VALUES ('#{Time.zone.now.beginning_of_minute.to_s(:db)}', '#{Time.zone.now.to_s(:db)}', '#{Time.zone.now.to_s(:db)}')
-    SQL
+    ClockworkTick.create!(processed_at: Time.zone.now.beginning_of_minute)
   end
 
   def self.down
